f96c29d8326dd1715a543584bd70c61c9234a967
Ilya Avdeev <ilya.avdeev@appricot.ru>
2026-02-11 14:08:45 +0300

    FOUNDATION-62: Fixed linter

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:startup_mvp_starter_flutter/auth/auth_service/auth_service.dart';
 import 'package:startup_mvp_starter_flutter/firebase_options/dev/firebase_options.dart';
 import 'package:startup_mvp_starter_flutter/my_app.dart';
 import 'package:startup_mvp_starter_flutter/utils/app_config.dart';
